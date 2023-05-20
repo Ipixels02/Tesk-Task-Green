8d51b1318dd2cc61b81de87cd54931ed62c087e6
Ipixels02 <ar4iking19@gmail.com>
2023-05-21 02:26:43 +0500

    fix index tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import {Provider} from "react-redux";
 import {setupStore} from "./shared/services/store/store";
@@ -7,11 +7,14 @@ import {BrowserRouter} from "react-router-dom";
 
 const store = setupStore()
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+);
+
+root.render(
     <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
     </BrowserRouter>,
-    document.getElementById('root')
 );
